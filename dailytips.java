39b2c6f4299b35a3752c3ec9c2dd27930586dffc
Alexander <aclittle@mta.ca>
2019-02-02 16:26:10 -0400

    Finished up food object, daily tips, and list of tips

---
+++ dailytips.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class dailytips {
+
+  public static void main(String[] args) {
+    Random rand = new Random;
+    int randomIndex = rand.nextInt();
+
+    ArrayList<String> theTips = new ArrayList<>();
+
+    System.out.println(theTips.get(randomIndex));
+  }
+
+}
