8b494f8733f168e76f15baa159e29e94c6df82c6
Alexander <aclittle@mta.ca>
2019-02-02 18:39:00 -0400

    Reviewed and finished the daily tips class

--- dailytips.java
+++ dailytips.java
@@ -1,12 +1,26 @@
 import java.util.*;
+import java.io.*;
 
 public class dailytips {
 
   public static void main(String[] args) {
-    Random rand = new Random;
-    int randomIndex = rand.nextInt();
-
+    Random rand = new Random();
+    int randomIndex = rand.nextInt(11);
     ArrayList<String> theTips = new ArrayList<>();
+
+    try {
+      File file = new File("tips.txt");
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+
+      String line = "";
+      line = reader.readLine();
+      while (line != null) {
+        theTips.add(line);
+        line = reader.readLine();
+      }
+    } catch (IOException e) {
+        System.out.println("It appears the file necessary for the daily tips is missing and cannot be found.");
+    }
 
     System.out.println(theTips.get(randomIndex));
   }
